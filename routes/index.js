85508095f68566c57813516dd840fa48748845ff
Anna Kurasova <drannakurasova@gmail.com>
2023-08-05 20:07:42 +0200

    iterations done but with extra elements idk why:(

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require("../models/Movie.model.js")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// LIST THE MOVIES
+router.get ("/movies", (req, res, next) =>{
+ Movie.find()
+ .select({title:1, image:1})
+ .then ((allthemovies)=> { 
+    // console.log (allthemovies)
+  res.render("movies.hbs", {
+    title: allthemovies,
+    image: allthemovies,
+  })
+}) 
+.catch ( (error) => {
+    next (error)
+})
+
+})
+
+//ONE MOVIE DESCRIPTION
+
+router.get ("/movies/:movieID", (req, res, next) => {
+//  let movieID = req.params;
+//  console.log (movieID)
+
+ let movieID= req.params.movieID;
+  console.log (movieID)
+
+ Movie.findById (movieID)
+ .then ( (oneMovieID)=>{
+    console.log (oneMovieID)
+    res.render ("movie-id.hbs", {
+        title: oneMovieID,
+        director: oneMovieID,
+        stars: [oneMovieID],
+        description: oneMovieID,
+        showtimes: [oneMovieID]
+    })
+
+ })
+ .catch ( (error)=>{
+    next (error)
+ })
+
+} )
 
 module.exports = router;
